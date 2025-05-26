91ed8e79a33a770d0f27d875524824a98f168d38
Michael Brecht <brecht.michael.b@gmail.com>
2025-05-26 09:34:21 -0700

    feat: add pageview tracking

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,11 @@
 import { getConfig } from "./config";
 import initPulse from "./initPulse";
 import trackEvent from "./trackEvent";
+import trackPageView from "./trackPageView";
 
 export default {
   getConfig,
   initPulse,
   trackEvent,
+  trackPageView,
 };
